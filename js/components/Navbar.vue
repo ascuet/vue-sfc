efa4fd0c756700170d932e1a28d08e38c90e1806
anik sen <aniksen.cuet09@gmail.com>
2020-03-08 19:54:46 +0600

    integrate vue single file component without cli project

---
+++ js/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+    <img class="rounded-circle" height="40" width="40" :src="image" alt="">
+    VueJS
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navbarNav"  aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+  </button>
+  
+</nav>
+
+</template>
+
+<script>
+module.exports = {
+
+  data() {
+  	return {
+  		image: '../static/images/nav.png'
+  	}
+  },	
+
+  mounted () {
+    console.log('this is a navbar')
+  }
+};
+</script>
+
+<style scoped>
+span {
+  background-color: yellow;
+}
+</style>
---
+++ js/components/Test.vue
@@ -0,0 +1,62 @@
+<template>
+	<div >
+		<table class="table">
+			<thead class="thead-dark">
+				<tr>
+					<th scope="col">ID</th>
+					<th scope="col">First Name</th>
+					<th scope="col">Last Name</th>
+					<th scope="col">Email</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="employee in employees">
+					<th scope="row"> {{ employee.id }} </th>
+					<td> {{ employee.f_name }} </td>
+					<td> {{ employee.l_name }} </td>
+					<td> {{ employee.email }} </td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</template>
+
+<script>
+module.exports = {
+
+	data() {
+		return {
+			employees: [
+				{
+					id: 1,
+					f_name: 'Anik',
+					l_name: 'Sen',
+					email: 'ascuet@gmail.com'
+				},
+				{
+					id: 2,
+					f_name: 'Minhaz',
+					l_name: 'Hossain',
+					email: 'minhaz@gmail.com'
+				},
+				{
+					id: 3,
+					f_name: 'Fahad',
+					l_name: 'Bin Munir',
+					email : 'fahadbinmnr@gmail.com'
+				}
+			]
+		}
+	},	
+
+	mounted () {
+		console.log('Hello World')
+	}
+};
+</script>
+
+<style scoped>
+span {
+	background-color: yellow;
+}
+</style>
